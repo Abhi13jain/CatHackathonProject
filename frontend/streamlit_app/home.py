23a08fabfbfcd67fca3cab4b0a48c7739b4d79d5
Abhi13jain <abhiijain13@gmail.com>
2025-08-28 22:03:35 +0530

    chore: initialise streamlit app

---
+++ frontend/streamlit_app/home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Smart Rental Tracker", layout="wide")
+st.title("Welcome to Smart Rental Tracker")
+st.info("Login page will be added here in next step.")
